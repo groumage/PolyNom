439c7774c98a8192af5596794e6cdfa9425f2ae3
guillaume <guillaume.roumage@gmail.com>
2023-08-06 07:09:22 -0400

    chore: add releaser script

---
+++ releaser.sh
@@ -0,0 +1,14 @@
+# /bin/bash
+
+version=$(git-conventional-commits version)
+echo "New version: $version"
+
+# update project version in files
+sed -i "s/[0-9]\+\(\.[0-9]\+\)\.\+[0-9]/$version/g" README.md
+
+git add README.md
+git commit -m "build: Bump project version to $version"
+git-conventional-commits changelog --release $version --file 'CHANGELOG.md'
+git add CHANGELOG.md
+git commit -m "docs: Create $version change log entry"
+git tag "v$version"
