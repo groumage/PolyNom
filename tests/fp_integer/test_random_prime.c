644bceed70f96334a7dea7556e6c48e59ce1e601
guillaume <guillaume.roumage@gmail.com>
2023-08-23 16:10:12 -0400

    test: add unit tests for random prime

--- tests/fp_integer/test_random_prime.c
+++ tests/fp_integer/test_random_prime.c
@@ -1,13 +1,16 @@
 #include "../../lib/include/fp_integer.h"
 
-int main()
+int main(int argc, char const *argv[])
 {
     FILE *file = fopen("../../../tests/fp_integer/input_test/random_prime.txt", "w");
     for (uint8_t i = 0; i < 10; i++)
     {
         mpz_t p;
         mpz_init(p);
-        random_prime(p, 100);
+        if (atoi(argv[1]) == 1)
+            random_prime_mpz(p, 10);
+        else
+            random_prime_mpz(p, 100);
         fwrite(mpz_get_str(NULL, 10, p), sizeof(char), mpz_sizeinbase(p, 10), file);
         fwrite("\n", sizeof(char), 1, file);
     }
