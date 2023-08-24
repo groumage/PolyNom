0a5e4a319b6e2cae693c6515917da8714c0d6c3c
guillaume <guillaume.roumage@gmail.com>
2023-08-24 19:30:21 -0400

    ci: update script to execute unit tests

--- test.sh
+++ test.sh
@@ -12,9 +12,10 @@ fi
 
 if [[ "$1" = "parse" ]]
 then
-    cd ../../build
+    cd build
     make
-    ctest --output-on-failure -L normal short-irreducible -LE long-irreducible > ctest_results.txt
+    ctest --output-on-failure -L parse > ctest_results.txt
+    cat ctest_results.txt
     if ! grep -q "100% tests passed" ctest_results.txt
     then
         echo "Parse test failed"
@@ -27,18 +28,20 @@ fi
 
 if [[ "$1" = "short" ]]
 then
-    cd tests/fp_poly/
-    touch arg_pari_gp.gp
-    echo -n short_test=1 > arg_pari_gp.gp
-    gp -q arg_pari_gp.gp generate_input.gp
-    rm arg_pari_gp.gp
-    cd ../../tests/fp_integer/
-    gp -q generate_input_integer.gp
-    cd ../../build
+    cd build
     make
+    cd ../
     for ((i=1; i<=$2; i++))
     do
         echo "Round $i of test is running..."
+        cd tests/fp_poly/
+        touch arg_pari_gp.gp
+        echo -n short_test=1 > arg_pari_gp.gp
+        gp -q arg_pari_gp.gp generate_input.gp
+        rm arg_pari_gp.gp
+        cd ../../tests/fp_integer/
+        gp -q generate_input_integer.gp
+        cd ../../build
         ctest --output-on-failure -L "nominal|short-polynom|short-integer" -LE "memory|long-integer|long-polynom" > ctest_results.txt
         cat ctest_results.txt
         if ! grep -q "100% tests passed" ctest_results.txt
@@ -49,24 +52,27 @@ then
         fi
         rm ctest_results.txt
         echo "Round $i of tests succeed"
+        cd ../
     done
     echo "All round tests succeed!"
 fi
 
 if [[ "$1" = "short-memory" ]]
 then
-    cd tests/fp_poly/
-    touch arg_pari_gp.gp
-    echo -n short_test=1 > arg_pari_gp.gp
-    gp -q arg_pari_gp.gp generate_input.gp
-    rm arg_pari_gp.gp
-    cd ../../tests/fp_integer/
-    gp -q generate_input_integer.gp
-    cd ../../build
+    cd build
     make
+    cd ../
     for ((i=1; i<=$2; i++))
     do
         echo "Round $i of test is running..."
+        cd tests/fp_poly/
+        touch arg_pari_gp.gp
+        echo -n short_test=1 > arg_pari_gp.gp
+        gp -q arg_pari_gp.gp generate_input.gp
+        rm arg_pari_gp.gp
+        cd ../../tests/fp_integer/
+        gp -q generate_input_integer.gp
+        cd ../../build
         ctest --output-on-failure -L "memory|short-polynom|short-integer" -LE "nominal|long-integer|long-polynom" > ctest_results.txt
         cat ctest_results.txt
         if ! grep -q "100% tests passed" ctest_results.txt
@@ -77,23 +83,26 @@ then
         fi
         rm ctest_results.txt
         echo "Round $i of tests succeed"
+        cd ../
     done
 fi
 
 if [[ "$1" = "long" ]]
 then
-    cd tests/fp_poly/
-    touch arg_pari_gp.gp
-    echo -n short_test=0 > arg_pari_gp.gp
-    gp -q arg_pari_gp.gp generate_input.gp
-    rm arg_pari_gp.gp
-    cd ../../tests/fp_integer/
-    gp -q generate_input_integer.gp
-    cd ../../build
+    cd build
     make
+    cd ../
     for ((i=1; i<=$2; i++))
     do
         echo "Round $i of test is running..."
+        cd tests/fp_poly/
+        touch arg_pari_gp.gp
+        echo -n short_test=0 > arg_pari_gp.gp
+        gp -q arg_pari_gp.gp generate_input.gp
+        rm arg_pari_gp.gp
+        cd ../../tests/fp_integer/
+        gp -q generate_input_integer.gp
+        cd ../../build
         ctest --output-on-failure -L "nominal|long-polynom|long-integer" -LE "memory|short-integer|short-polynom" > ctest_results.txt
         cat ctest_results.txt
         if ! grep -q "100% tests passed" ctest_results.txt
@@ -104,23 +113,26 @@ then
         fi
         rm ctest_results.txt
         echo "Round $i of tests succeed"
+        cd ../
     done
 fi
 
 if [[ "$1" = "long-memory" ]]
 then
-    cd tests/fp_poly/
-    touch arg_pari_gp.gp
-    echo -n short_test=0 > arg_pari_gp.gp
-    gp -q arg_pari_gp.gp generate_input.gp
-    rm arg_pari_gp.gp
-    cd ../../tests/fp_integer/
-    gp -q generate_input_integer.gp
-    cd ../../build
+    cd build
     make
+    cd ../
     for ((i=1; i<=$2; i++))
     do
         echo "Round $i of test is running..."
+        cd tests/fp_poly/
+        touch arg_pari_gp.gp
+        echo -n short_test=0 > arg_pari_gp.gp
+        gp -q arg_pari_gp.gp generate_input.gp
+        rm arg_pari_gp.gp
+        cd ../../tests/fp_integer/
+        gp -q generate_input_integer.gp
+        cd ../../build
         ctest --output-on-failure -L "memory|long-polynom|long-integer" -LE "nominal|short-integer|short-polynom" > ctest_results.txt
         cat ctest_results.txt
         if ! grep -q "100% tests passed" ctest_results.txt
@@ -131,5 +143,6 @@ then
         fi
         rm ctest_results.txt
         echo "Round $i of tests succeed"
+        cd ../
     done
 fi
