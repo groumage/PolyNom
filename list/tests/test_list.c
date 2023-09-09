011eaef8e8bebb4f3f8cd4c819f75cf61107e0f7
guillaume <guillaume.roumage@gmail.com>
2023-09-09 09:10:14 -0400

    test: more exhaustive unit tests for list library

--- list/tests/test_list.c
+++ list/tests/test_list.c
@@ -40,19 +40,33 @@ int main()
     assert (list_assert(list, (uint8_t []) {1, 6}, 2) == LIST_E_SUCCESS);
     assert (list_remove_coeff(list, 6) == LIST_E_SUCCESS);
     assert (list_assert(list, (uint8_t []) {1}, 1) == LIST_E_SUCCESS);
+    assert (list_add_after(list, 2, list->head) == LIST_E_SUCCESS);
+    assert (list_assert(list, (uint8_t []) {1, 2}, 2) == LIST_E_SUCCESS);
+    assert (list_get_at_pos(list, 0)->coeff == 1);
+    assert (list_get_at_pos(list, 1)->coeff == 2);
+    assert (list_add_after(list, 3, list->head) == LIST_E_SUCCESS);
+    assert (list_assert(list, (uint8_t []) {1, 3, 2}, 3) == LIST_E_SUCCESS);
+    assert (list_get_pos(list, list->head) == 0);
+    assert (list_get_pos(list, list->head->next->next) == 2);
     assert (list_destroy(list) == LIST_E_SUCCESS);
 
-    // error cases
+    // error cases: null pointers
     assert (list_destroy(NULL) == LIST_E_NULL_PTR);
     assert (list_add_beginning(NULL, 0) == LIST_E_NULL_PTR);
     assert (list_add_end(NULL, 0) == LIST_E_NULL_PTR);
     assert (list_remove_node(NULL, NULL) == LIST_E_NULL_PTR);
     assert (list_remove_node(NULL, 0) == LIST_E_NULL_PTR);
     assert (list_remove_head(NULL) == LIST_E_NULL_PTR);
+    assert (list_remove_tail(NULL) == LIST_E_NULL_PTR);
     assert (list_add_at(NULL, 0, 0) == LIST_E_NULL_PTR);
     assert (list_print(NULL, NULL) == LIST_E_NULL_PTR);
     assert (list_assert(NULL, (uint8_t []) {1}, 1) == LIST_E_NULL_PTR);
     assert (list_create_from_array(NULL, 0) == NULL);
+    assert (list_add_after(NULL, 0, NULL) == LIST_E_NULL_PTR);
+    assert (list_get_pos(NULL, NULL) == LIST_E_NULL_PTR);
+    assert (list_get_at_pos(NULL, 0) == NULL);
+
+    // error cases: other
     list = list_init();
     assert (list_remove_coeff(list, 0) == LIST_E_EMPTY);
     assert (list_remove_node(list, 0) == LIST_E_EMPTY);
@@ -60,16 +74,20 @@ int main()
     assert (list_remove_tail(list) == LIST_E_EMPTY);
     assert (list_add_beginning(list, 1) == LIST_E_SUCCESS);
     assert (list_add_at(list, 2, 3) == LIST_E_INVALID_ARGUMENT);
+    assert (list_get_pos(list, NULL) == LIST_E_INVALID_ARGUMENT);
     assert (list_remove_coeff(NULL, 0) == LIST_E_NULL_PTR);
     assert (list_remove_coeff(list, 2) == LIST_E_NOT_FOUND);
     assert (list_assert(list, NULL, 1) == LIST_E_INVALID_ARGUMENT);
     assert (list_assert(list, (uint8_t []) {2}, 1) == LIST_E_INVALID_ARGUMENT);
+    assert (list_add_after(list, 0, NULL) == LIST_E_INVALID_ARGUMENT);
     list_t *list2 = list_init();
     assert (list_add_beginning(list2, 1) == LIST_E_SUCCESS);
     assert (list_assert(list2, (uint8_t []) {1}, 2) == LIST_E_INVALID_ARGUMENT);
+    assert (list_get_pos(list2, list->head) == LIST_E_NOT_FOUND);
     assert (list_remove_node(list, NULL) == LIST_E_INVALID_ARGUMENT);
     assert (list_remove_node(list, list2->head) == LIST_E_NOT_FOUND);
     assert (list_remove_tail(list2) == LIST_E_NOT_FOUND);
+    assert (list_get_at_pos(list, 2) == NULL);
     assert (list_destroy(list) == LIST_E_SUCCESS);
     assert (list_destroy(list2) == LIST_E_SUCCESS);
 
@@ -88,8 +106,6 @@ int main()
     fread(buffer, 1, sizeof(buffer), fp);
     fclose(fp);
     char expected_output[] = "3 2 1 \n";
-    fprintf(stderr, "%d\n", strcmp(buffer, expected_output));
-    printf("%d\n", strcmp(buffer, expected_output));
     assert (strcmp(buffer, expected_output) == 0);
     assert (list_destroy(list) == LIST_E_SUCCESS);
     remove(filename);
