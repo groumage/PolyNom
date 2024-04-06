0c1ad36ae8e12126315545d080e2e77b486c5335
guillaume <guillaume.roumage@gmail.com>
2024-04-06 19:26:59 +0200

    ci: add check for gcovr installation

--- coverage.sh
+++ coverage.sh
@@ -13,5 +13,11 @@ do
 done
 rm -rf coverage
 mkdir coverage
+# check gcovr is installed
+if ! command -v gcovr &> /dev/null
+then
+    echo "gcovr could not be found"
+    exit
+fi
 gcovr -r . --exclude tests/ -j 8 --html --html-details -o coverage/coverage.html
 firefox coverage/coverage.html
